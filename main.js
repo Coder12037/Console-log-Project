762149ffc13cc1fef46636b78047c89b7e7e1ba3
Coder12037 <93456786+Coder12037@users.noreply.github.com>
2022-07-27 10:22:13 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name="Ranbir";
+console.log (first_name)
+
+var last_name="Kapoor";
+console.log(last_name);
+
+var a= 4;
+var b= 5;
+
+var add= a+b;
+console.log (add);
+
+var subtract= a-b;
+console.log(subtract);
+
+var multiply= a*b;
+console.log(multiply);
